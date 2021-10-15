567ffcac5c9d2baeaa35f58d7566656f6649bd2e
dsolerh <dsolerh.cinter95@gmail.com>
2021-10-15 14:51:07 -0400

    up

--- CalculatorUnitTests.cs
+++ CalculatorUnitTests.cs
@@ -1,14 +1,41 @@
 using System;
 using Xunit;
+using calculator_lib;
 
 namespace cs_calculator_lib_test
 {
-    public class UnitTest1
+    public class CalculatorUnitTests
     {
         [Fact]
-        public void Test1()
+        public void TestAdd2And2()
         {
+            // arrange
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+            var calc = new Calculator();
 
+            // act
+            double result = calc.Add(a,b);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestAdd2And3()
+        {
+            // arrange
+            double a = 2;
+            double b = 3;
+            double expected = 5;
+            var calc = new Calculator();
+
+            // act
+            double result = calc.Add(a,b);
+
+            // assert
+            Assert.Equal(expected, result);
         }
     }
 }
